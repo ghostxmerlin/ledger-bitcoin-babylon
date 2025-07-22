03d2f5f0d507f439d1f913679dc26713d27f74b8
ghostxmerlin <ghostxmerlin@gmail.com>
2025-07-22 17:38:03 +0800

    add default signpsbt

--- src/__tests__/policy.test.ts
+++ src/__tests__/policy.test.ts
@@ -1,11 +1,14 @@
 import Transport from '@ledgerhq/hw-transport-node-speculos-http';
+import { AppClient,DefaultWalletPolicy,PsbtV2 } from '..';
 import { stakingTxPolicy } from '../lib/babylon/index';
 
 describe('stakingTxPolicy', () => {
   let transport: any;
+  let app: AppClient;
 
   beforeAll(async () => {
     transport = await Transport.open('http://127.0.0.1:5000' as any);
+    app = new AppClient(transport);
   });
 
   afterAll(async () => {
@@ -41,4 +44,29 @@ describe('stakingTxPolicy', () => {
     expect(policy).toBeDefined();
     expect(policy.descriptorTemplate).toBe('tr(@0/**)');
   });
+    it("can sign a psbt", async () => {
+        // psbt from test_sign_psbt_singlesig_wpkh_2to2 in the main test suite, converted to PSBTv2
+        // const psbtBuf = Buffer.from(
+        //   "cHNidP8BAIkCAAAAAQoDdUgOA5oDhvrH0NWZTa/GJzvd4UhFIbmbOiWufc84AAAAAAD/////AlDDAAAAAAAAIlEg12Pea0ceMFZBukHWXGeC6MvP9uCOg9qrDaEnW7yfqtAcAi0AAAAAACJRIHQO5k5FLjuu4SewPBlbzCGtPt3tLvJsWvSD2cVjBNHlAAAAAAABASvAxi0AAAAAACJRIHQO5k5FLjuu4SewPBlbzCGtPt3tLvJsWvSD2cVjBNHlARcg3I0vnv8MT0294HCkjjMO/JCLYqdmVo2R5ljyhLMkuHgAAAA=",
+        //   "base64"
+        // );
+        const psbtBuf = Buffer.from(
+           "cHNidP8BAH0CAAAAAU5oPucfQQOAdrEZwJODBvpzHfaA/orEXxwbxelbMexgAAAAAAD/////AsQJAAAAAAAAFgAUW+EmJNCKK0JAldfAciHDNFDRS/EEpgAAAAAAACJRICyVutUKY9E6qBjfjktoZBga2/RyCoiq+OPBI1ugik2fAAAAAAABAStQwwAAAAAAACJRINdj3mtHHjBWQbpB1lxngujLz/bgjoPaqw2hJ1u8n6rQQhXBUJKbdMGgSVS3i0tgNel6XgeKWg8o7JbVR7/ums6AOsCJtgX5iDHD5SbZ6yF5ZRRSk4qMD/f16u7MthJR1dRt6/15ASDcjS+e/wxPTb3gcKSOMw78kItip2ZWjZHmWPKEsyS4eK0g1mEk+PQv2D5MkBoQCuO11wbvbP0hewS8ZBUuc5owxB6tIAruBQmxbbccmZI4pIJ9uUVSaFmxPJVIerRnJTV8mp8lrCARPDoyqdMgtyGQoEoCCg2zl27zaXJnMljpo4o2Tz3DsLogF5Ic8VbMtOc9Qo+ZbtEbJFMT434nyXisTSzCHspGcuS6IDu5PfyLYYh9dx82MOmmPpfLr8/MeFVqR034OjGg74mcuiBAr69HxP+lbehkENjke6ortvBLYE9OokMjc33cP+CS37ogeacf/XHFA+8uL5G8z8j82nlG9GU87w2fPd4geV7zufC6INIfr3jGdRoNOOa9gCi5B/8H6ahppD/IN9az+N/2EZo2uiD1GZ764/KLuCR2Fjp+RYx61EXZv/sGgtENO9sstB+Ojrog+p2ILUX0BgvbgEIYOCjNh1RPHqmXOA5YbKt31f1phze6VpzAARcgUJKbdMGgSVS3i0tgNel6XgeKWg8o7JbVR7/ums6AOsAAAAA=",
+           "base64"
+         );
+  
+        //const automation = JSON.parse(fs.readFileSync('src/__tests__/automations/sign_with_wallet_accept.json').toString());
+       // await setSpeculosAutomation(transport, automation);
+    
+        const walletPolicy = new DefaultWalletPolicy(
+          "tr(@0/**)",
+          "[f5acc2fd/86'/1'/0']tpubDDKYE6BREvDsSWMazgHoyQWiJwYaDDYPbCFjYxN3HFXJP5fokeiK4hwK5tTLBNEDBwrDXn8cQ4v9b2xdW62Xr5yxoQdMu1v6c7UDXYVH27U"
+        );
+    
+        const psbt = new PsbtV2();
+        psbt.deserialize(psbtBuf);
+        const result = await app.signPsbt(psbt, walletPolicy, null, () => {});
+    
+        expect(result.length).toEqual(1);
+      });
 });
